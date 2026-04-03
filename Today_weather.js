b72392584cf5078ec9883bdde842e304089a4547
Tran Pham Thao Nguyen <tpthaonguyen04@gmail.com>
2026-04-03 13:47:23 +0700

    Do exercise Today's weather (exercise 1)
    In your JavaScript file, instantiate and assign the following variables with information about the current weather:
    temperature should hold a number representing the current temperature in Fahrenheit
    conditions should hold a string about the current weather conditions (i.e. "cloudy", "sunny", etc).
    sunriseTime should hold a string representing what time the sun rose this morning
    sunsetTime should hold a string representing what time the sun will set this evening
    Log each variable to the console.
    Use string template literals to combine these variables into a sentence about the weather. (Example: "It's currently sunny and 55 degrees. The sun rose this morning at 7:34 AM and will set at 5:09 PM.")

---
+++ Today_weather.js
@@ -0,0 +1,11 @@
+let temperature = 40;
+let conditions = "sunny";
+let sunriseTime = "7:30 AM";
+let sunsetTime = "5:30 PM";
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+console.log(`It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`);
